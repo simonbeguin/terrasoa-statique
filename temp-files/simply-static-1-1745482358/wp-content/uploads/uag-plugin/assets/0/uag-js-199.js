38bff1bf018776ea5e8960a992e58ba7d3314ec4
Simon <103170889+simonbeguin@users.noreply.github.com>
2025-04-24 10:34:00 +0200

    Initial export Simply Static

---
+++ temp-files/simply-static-1-1745482358/wp-content/uploads/uag-plugin/assets/0/uag-js-199.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-3142aeb2' );
+	UAGBTabs.anchorTabId( '.uagb-block-3142aeb2' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-3142aeb2' );
+}, false );
+ });
