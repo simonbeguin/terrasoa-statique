bc8c2ca1647f2bdac1963894769e19f2d6948f7a
Simon <103170889+simonbeguin@users.noreply.github.com>
2025-05-05 12:38:41 +0200

    new updates for mobile

--- temp-files/simply-static-1-1745482358/wp-content/uploads/uag-plugin/assets/0/uag-js-199.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBTabs.init( '.uagb-block-3142aeb2' );
-	UAGBTabs.anchorTabId( '.uagb-block-3142aeb2' );
-});
-window.addEventListener( 'hashchange', function() {
-	UAGBTabs.anchorTabId( '.uagb-block-3142aeb2' );
-}, false );
- });
